2cfa36dd14a375af4f8c0bb759ed667c22f15a6f
Bruno Antunes <bmvantunes@gmail.com>
2020-01-16 20:51:41 +0000

    You are loading @emotion/react when it is already loaded. Running multiple instances may cause problems. This can happen if multiple versions are used, or if multiple builds of the same version are used.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+jest.mock('./ComponentParent');
+
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const test = getByText(/Test/i);
+  expect(test).toBeInTheDocument();
 });
---
+++ src/ComponentParent.tsx
@@ -0,0 +1,6 @@
+import { ComponentChild } from "./ComponentChild";
+import styled from "@emotion/styled";
+
+export const ComponentParent = styled(ComponentChild)`
+  background: blue;
+`;
